0631846be551c1b14752f008d1151cc6589e6b5e
hhhxxx11 <151365895+hhhxxx11@users.noreply.github.com>
2024-03-28 21:14:23 +0800

    Update main.py

--- WeiXinPost-main/main.py
+++ WeiXinPost-main/main.py
@@ -76,7 +76,11 @@ def get_Today_Class():
     month = localtime().tm_mon
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
-    todayClasses = get_Week_Classes(None)[today.weekday()]
+    week_classes = get_Week_Classes(None)
+    if week_classes is not None:
+        todayClasses = week_classes[today.weekday()]
+    else:
+        todayClasses = None  # 或者根据实际情况返回一个默认值
     return todayClasses
 
 
@@ -87,28 +91,28 @@ def get_Class(day):
 
 
 # # 发送本周所有课程，周一的时候发
-# def send_Week_Classes(to_user, access_token, week):
-#     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
-#     theuser = to_user[0]
-#     data = {
-#         "touser": theuser,
-#         "template_id": config.template_id2,
-#         "url": "http://weixin.qq.com/download",
-#         "topcolor": "#FF0000",
-#         "data": {
-#             "weeks": {
-#                 "value": classInfo,
-#                 "color": "#FF8000"
-#             }
-#         }
-#     }
-#     headers = {
-#         'Content-Type': 'application/json',
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-#                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-#     }
-#     response = post(url, headers=headers, json=data)
-#     print(response.text)
+ def send_Week_Classes(to_user, access_token, week):
+     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+     theuser = to_user[0]
+     data = {
+         "touser": theuser,
+         "template_id": config.template_id2,
+         "url": "http://weixin.qq.com/download",
+         "topcolor": "#FF0000",
+         "data": {
+             "weeks": {
+                 "value": classInfo,
+                 "color": "#FF8000"
+             }
+         }
+     }
+     headers = {
+         'Content-Type': 'application/json',
+         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+     }
+     response = post(url, headers=headers, json=data)
+     print(response.text)
 
 
 # 发送每日信息
